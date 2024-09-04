04ad35e7fb470464496c8619bc50ab44f9cc6318
Ben Simms <ben@bensimms.moe>
2024-09-04 17:23:47 +0200

    Flake improvements

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
 
     crane = {
       url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     fenix = {
@@ -31,34 +30,59 @@
       ];
       perSystem = { config, pkgs, system, lib, ... }:
         let
-          arm-toolchain = fenix.packages.${system}.fromToolchainFile {
+          arm-toolchain-plain = fenix.packages.${system}.fromToolchainFile {
             file = ./rust-toolchain.toml;
-            sha256 = "sha256-Aoc5PJYT5h7tE6joA5+6yusOBCzQxSf/18hm7PDmZCI=";
+            sha256 = "sha256-SKrgSoah/i97Qng3btr3l2ax9yn2eftY0+yofsPNkoY=";
           };
-          native-toolchain = fenix.packages.${system}.complete.withComponents [
+          native-toolchain = (fenix.packages.${system}.complete.withComponents [
             "cargo"
             "clippy"
-            "rust-src"
-            "rustc"
-            "rustfmt"
-          ];
+            # "rust-src"
+            # "rustc"
+            # "rustfmt"
+          ]);
+          arm-toolchain = pkgs.runCommand "turbowaker-rust" { } ''
+              echo "test $out ${arm-toolchain-plain}"
+              cp -RL ${arm-toolchain-plain} $out
+              chmod -R +rwx $out
+
+              echo "doing patch"
+
+              patch $out/lib/rustlib/src/rust/library/core/Cargo.toml ${./turbowaker/Cargo.toml.patch}
+              patch $out/lib/rustlib/src/rust/library/core/src/task/wake.rs ${./turbowaker/wake.rs.patch}
+            '';
+          
           toolchain = fenix.packages.${system}.combine [ arm-toolchain native-toolchain ];
           craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
-          package = { path, target ? "thumbv6m-none-eabi", args ? "", profile ? "release" }: craneLib.buildPackage {
-            cargoExtraArgs = "--target ${target} ${args}";
+
+          src = craneLib.cleanCargoSource ./.;
+          
+          package = { target ? "thumbv6m-none-eabi", args ? "", profile ? "release" }: craneLib.buildPackage {
+            inherit src;
+          
+            cargoVendorDir = craneLib.vendorMultipleCargoDeps {
+              inherit (craneLib.findCargoFiles src) cargoConfigs;
+              cargoLockList = [
+                ./Cargo.lock
+
+                # Unfortunately this approach requires IFD (import-from-derivation)
+                # otherwise Nix will refuse to read the Cargo.lock from our toolchain
+                # (unless we build with `--impure`).
+                #
+                # Another way around this is to manually copy the rustlib `Cargo.lock`
+                # to the repo and import it with `./path/to/rustlib/Cargo.lock` which
+                # will avoid IFD entirely but will require manually keeping the file
+                # up to date!
+                "${toolchain}/lib/rustlib/src/rust/Cargo.lock"
+              ];
+            };
+
+            cargoExtraArgs = "-Z build-std=core,panic_abort,alloc -Z build-std-features=panic_immediate_abort,core/turbowakers --target ${target} ${args}";
             CARGO_PROFILE = profile;
             pname = "rusty-dilemma";
             version = "0.1.0";
-            src = lib.cleanSourceWith {
-              src = craneLib.path path;
-              filter =
-                let
-                  embeddedStuffFilter = path: _type: builtins.match ".*\\.x$" path != null;
-                  embeddedOrCargoStuff = path: type:
-                    (embeddedStuffFilter path type) || (craneLib.filterCargoSources path type);
-                in
-                embeddedOrCargoStuff;
-            };
+            
+            strictDeps = true;
             doCheck = false;
             buildInputs = [
               # Add additional build inputs here
@@ -67,7 +91,6 @@
               pkgs.libiconv
             ];
           };
-          firmware = args: package (args // { path = ./.; });
           elf = pkg: name: pkgs.runCommandLocal "mkelf" { } ''
             mkdir -p $out
             cp ${pkg}/bin/${name} $out/${name}.elf
@@ -79,33 +102,37 @@
         in
         rec
         {
-          devShells.default = pkgs.mkShell {
-            # inputsFrom = [ (firmware { args = "--lib"; profile = "dev"; }) ];
-            nativeBuildInputs = with pkgs; [
-              # fenix.packages.${system}.rust-analyzer
-              # cargo-binutils
-              # probe-rs
-              picotool
-              # pkgsCross.arm-embedded.buildPackages.binutils
-            ];
+          devShells.default = craneLib.devShell {
+            packages = with pkgs; [ picotool libiconv ];
           };
-          packages.default = firmware { args = "--lib"; profile = "dev"; };
-          packages.bin = firmware { args = "--bin binary --no-default-features"; profile = "release"; };
-          packages.debug-bin = firmware { args = "--bin binary --features probe,m2"; profile = "dev"; };
-          packages.binaries = pkgs.symlinkJoin {
-            name = "dilemma-binaries";
-            paths = [
-              (elf packages.bin "binary")
-              (binary packages.bin "binary")
-            ];
-          };
-          packages.debug-binaries = pkgs.symlinkJoin {
-            name = "dilemma-binaries";
-            paths = [
-              (elf packages.debug-bin "binary")
-              (binary packages.debug-bin "binary")
-            ];
-          };
+          # binaries don't currently work because we need the macros package to be built with std supported, crane seems to do some weird stuff that makes it forced onto a no-std build env
+          # devShells.default = pkgs.mkShell {
+          #   # inputsFrom = [ (firmware { args = "--lib"; profile = "dev"; }) ];
+          #   nativeBuildInputs = with pkgs; [
+          #     # fenix.packages.${system}.rust-analyzer
+          #     # cargo-binutils
+          #     # probe-rs
+          #     picotool
+          #     # pkgsCross.arm-embedded.buildPackages.binutils
+          #   ];
+          # };
+        #   packages.default = package { args = "--lib"; profile = "dev"; };
+        #   packages.bin = package { args = "--bin binary --no-default-features"; profile = "release"; };
+        #   packages.debug-bin = package { args = "--bin binary --features probe,m2"; profile = "dev"; };
+        #   packages.binaries = pkgs.symlinkJoin {
+        #     name = "dilemma-binaries";
+        #     paths = [
+        #       (elf packages.bin "binary")
+        #       (binary packages.bin "binary")
+        #     ];
+        #   };
+        #   packages.debug-binaries = pkgs.symlinkJoin {
+        #     name = "dilemma-binaries";
+        #     paths = [
+        #       (elf packages.debug-bin "binary")
+        #       (binary packages.debug-bin "binary")
+        #     ];
+        #   };
         };
     };
 }
